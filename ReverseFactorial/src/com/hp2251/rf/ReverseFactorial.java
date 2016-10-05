eb38608bd03924a554d0176925eec7991e8d411a
hp2251 <hp2251.dev@gmail.com>
2016-10-04 20:44:58 -0400

    implemented reverse factorial

--- ReverseFactorial/src/com/hp2251/rf/ReverseFactorial.java
+++ ReverseFactorial/src/com/hp2251/rf/ReverseFactorial.java
@@ -1,10 +1,34 @@
 package com.hp2251.rf;
 
+import java.io.PrintStream;
+
 public class ReverseFactorial {
+	private static final PrintStream OUT = System.out;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(ReverseFactorial.ReverseF(120));
+		System.out.println(ReverseFactorial.ReverseF(121));
+	}
 
+	public static String ReverseF(int n) {
+		int result = n;
+		int divisor = 2;
+		
+		while(result % divisor == 0) {
+			result = result / divisor++;
+		}
+		
+		StringBuilder answer = new StringBuilder(n + "");
+		answer.append(" = ");
+		
+		if(result == 1) answer.append((divisor-1)).append("!");
+		else answer.append("NONE");
+		
+		return answer.toString();
+	}
+	
+	public static String Factorial(int n) {
+		return "";
 	}
 
 }
